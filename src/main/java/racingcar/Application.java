2066d8fe09ca6f32526ab67a4c842dbeced8b191
Leafurya <audrb5300@naver.com>
2025-10-24 23:59:38 +0900

    feat: 사용자 입력 받기 구현
    ### 사용자 입력 받기
    - 경주할 **자동차 이름**을 입력받는다.
        - 자동차 이름은 쉼표(`,`)로 구분한다.
        - 각 이름은 **5자 이하**만 가능하다.
        - 형식이 올바르지 않을 경우 `IllegalArgumentException`을 발생시키고 프로그램을 종료한다.
    - **이동 횟수**를 입력받는다.
        - 이동 횟수는 숫자여야 한다.
        - 잘못된 값이 들어올 경우 `IllegalArgumentException`을 발생시키고 프로그램을 종료한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,37 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    private static String[] carNames=null;
+    private static int raceCount;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        getCarNames();
+        getRaceCount();
+    }
+    private static void inspectCarNames(){
+        for(String name:carNames){
+            if(!name.matches("^[a-zA-Z]{1,5}$")){
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+    private static void getCarNames(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        String strCarNames=Console.readLine();
+
+        carNames=strCarNames.split(",");;
+        inspectCarNames();
+    }
+    private static void getRaceCount(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String strRaceCount=Console.readLine();
+        try{
+            raceCount=Integer.parseInt(strRaceCount);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException();
+        }
     }
 }
