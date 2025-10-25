0fe51e2cfa9a8fe5c25e5c699eb34aa528e9b9d5
Leafurya <audrb5300@naver.com>
2025-10-25 14:42:06 +0900

    style: 코드 스타일 반영

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 
 public class Application {
-    private static String[] carNames=null;
+    private static String[] carNames = null;
     private static int raceCount;
     private static Car[] cars;
 
@@ -20,58 +20,65 @@ public class Application {
 
         determineWinner();
     }
-    private static void determineWinner(){
-        int max=0;
-        ArrayList<String> winners=new ArrayList<>();
-        for(Car car:cars){
-            int distance=car.getDistance();
-            if(max<distance){
-                max=distance;
+
+    private static void determineWinner() {
+        int max = 0;
+        ArrayList<String> winners = new ArrayList<>();
+        for (Car car : cars) {
+            int distance = car.getDistance();
+            if (max < distance) {
+                max = distance;
             }
         }
 
-        for(Car car:cars){
-            if(max==car.getDistance()){
+        for (Car car : cars) {
+            if (max == car.getDistance()) {
                 winners.add(car.getName());
             }
         }
-        System.out.println("최종 우승자 : "+String.join(", ",winners));
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
     }
-    private static void startRacing(){
-        for(int i=0;i<raceCount;i++){
-            for(Car car:cars){
+
+    private static void startRacing() {
+        for (int i = 0; i < raceCount; i++) {
+            for (Car car : cars) {
                 car.race();
             }
             System.out.println();
         }
     }
-    private static void createCars(){
-        cars=new Car[carNames.length];
-        int i=0;
-        for(String name:carNames){
-            cars[i++]=new Car(name);
+
+    private static void createCars() {
+        cars = new Car[carNames.length];
+        int i = 0;
+        for (String name : carNames) {
+            cars[i++] = new Car(name);
         }
     }
-    private static void inspectCarNames(){
-        for(String name:carNames){
-            if(!name.matches("^[a-zA-Z]{1,5}$")){
+
+    private static void inspectCarNames() {
+        for (String name : carNames) {
+            if (!name.matches("^[a-zA-Z]{1,5}$")) {
                 throw new IllegalArgumentException();
             }
         }
     }
-    private static void getCarNames(){
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String strCarNames=Console.readLine();
 
-        carNames=strCarNames.split(",");;
+    private static void getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String strCarNames = Console.readLine();
+
+        carNames = strCarNames.split(",");
+        ;
         inspectCarNames();
     }
-    private static void getRaceCount(){
+
+    private static void getRaceCount() {
         System.out.println("시도할 횟수는 몇 회인가요?");
-        String strRaceCount=Console.readLine();
-        try{
-            raceCount=Integer.parseInt(strRaceCount);
-        }catch (NumberFormatException e){
+        String strRaceCount = Console.readLine();
+        try {
+            raceCount = Integer.parseInt(strRaceCount);
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException();
         }
     }
--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -6,25 +6,28 @@ public class Car {
     private String name;
     private int distance;
 
-    public Car(String name){
-        this.name=name;
-        distance=0;
+    public Car(String name) {
+        this.name = name;
+        distance = 0;
     }
-    public void race(){
-        int rand=Randoms.pickNumberInRange(0,9);
-        if(rand>=4){
+
+    public void race() {
+        int rand = Randoms.pickNumberInRange(0, 9);
+        if (rand >= 4) {
             distance++;
         }
-        System.out.print(name+" : ");
-        for(int i=0;i<distance;i++){
+        System.out.print(name + " : ");
+        for (int i = 0; i < distance; i++) {
             System.out.print("-");
         }
         System.out.println();
     }
-    public int getDistance(){
+
+    public int getDistance() {
         return distance;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
 }
